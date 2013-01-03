e1e86c6e2501c5dad797b61dbfef0be4f5dbaa67
Rich Siegel <rsiegel@ise.com>
2013-01-03 11:43:24 -0500

    group add remove passes

--- lib/mscs.rb
+++ lib/mscs.rb
@@ -26,6 +26,7 @@ def utf16le_to_usascii(my_str)
 end
 
 #cluster enumeration constants (bitmask)
+# http://msdn.microsoft.com/en-us/library/windows/desktop/aa368834(v=vs.85).aspx
 CLUSTER_ENUM_NODE=1
 CLUSTER_ENUM_RESTYPE=2
 CLUSTER_ENUM_RESOURCE=4
@@ -36,34 +37,133 @@ CLUSTER_ENUM_SHARED_VOLUME_RESOURCE=1073741824
 CLUSTER_ENUM_INTERNAL_NETWORK=2147483648 
 
 #clustergroup enumeration constants
+# http://msdn.microsoft.com/en-us/library/windows/desktop/bb309149(v=vs.85).aspx
 CLUSTER_GROUP_ENUM_CONTAINS=1
 CLUSTER_GROUP_ENUM_NODES=2
 CLUSTER_GROUP_ENUM_ALL=3
 
 #cluster resource enumeration constants
+# http://msdn.microsoft.com/en-us/library/windows/desktop/aa369028(v=vs.85).aspx
+
 CLUSTER_RESOURCE_ENUM_DEPENDS=1
 CLUSTER_RESOURCE_ENUM_PROVIDES=2
 CLUSTER_RESOURCE_ENUM_NODES=4
 
+# http://msdn.microsoft.com/en-us/library/windows/desktop/cc307921(v=vs.85).aspx
+# cluster control codes
+CLUSCTL_RESOURCE_UNKNOWN                           = 0x01000000
+CLUSCTL_RESOURCE_GET_CHARACTERISTICS               = 0x01000005
+CLUSCTL_RESOURCE_GET_FLAGS                         = 0x01000009
+CLUSCTL_RESOURCE_GET_CLASS_INFO                    = 0x0100000d
+CLUSCTL_RESOURCE_GET_REQUIRED_DEPENDENCIES         = 0x01000011
+CLUSCTL_RESOURCE_GET_NAME                          = 0x01000029
+CLUSCTL_RESOURCE_GET_ID                            = 0x01000039
+CLUSCTL_RESOURCE_GET_RESOURCE_TYPE                 = 0x0100002d
+CLUSCTL_RESOURCE_ENUM_COMMON_PROPERTIES            = 0x01000051
+CLUSCTL_RESOURCE_GET_RO_COMMON_PROPERTIES          = 0x01000055
+CLUSCTL_RESOURCE_GET_COMMON_PROPERTIES             = 0x01000059
+CLUSCTL_RESOURCE_SET_COMMON_PROPERTIES             = 0x0140005e
+CLUSCTL_RESOURCE_VALIDATE_COMMON_PROPERTIES        = 0x01000061
+CLUSCTL_RESOURCE_GET_COMMON_PROPERTY_FMTS          = 0x01000065
+CLUSCTL_RESOURCE_ENUM_PRIVATE_PROPERTIES           = 0x01000079
+CLUSCTL_RESOURCE_GET_RO_PRIVATE_PROPERTIES         = 0x0100007d
+CLUSCTL_RESOURCE_GET_PRIVATE_PROPERTIES            = 0x01000081
+CLUSCTL_RESOURCE_SET_PRIVATE_PROPERTIES            = 0x01400086
+CLUSCTL_RESOURCE_VALIDATE_PRIVATE_PROPERTIES       = 0x01000089
+CLUSCTL_RESOURCE_GET_PRIVATE_PROPERTY_FMTS         = 0x0100008d
+CLUSCTL_RESOURCE_ADD_REGISTRY_CHECKPOINT           = 0x014000a2
+CLUSCTL_RESOURCE_DELETE_REGISTRY_CHECKPOINT        = 0x014000a6
+CLUSCTL_RESOURCE_GET_REGISTRY_CHECKPOINTS          = 0x010000a9
+CLUSCTL_RESOURCE_ADD_CRYPTO_CHECKPOINT             = 0x014000ae
+CLUSCTL_RESOURCE_DELETE_CRYPTO_CHECKPOINT          = 0x014000b2
+CLUSCTL_RESOURCE_GET_CRYPTO_CHECKPOINTS            = 0x010000b5
+CLUSCTL_RESOURCE_GET_LOADBAL_PROCESS_LIST          = 0x010000c9
+CLUSCTL_RESOURCE_GET_NETWORK_NAME                  = 0x01000169
+CLUSCTL_RESOURCE_NETNAME_GET_VIRTUAL_SERVER_TOKEN  = 0x0100016d
+CLUSCTL_RESOURCE_NETNAME_SET_PWD_INFO              = 0x0100017a
+CLUSCTL_RESOURCE_NETNAME_DELETE_CO                 = 0x0100017e
+CLUSCTL_RESOURCE_NETNAME_VALIDATE_VCO              = 0x01000181
+CLUSCTL_RESOURCE_NETNAME_RESET_VCO                 = 0x01000185
+CLUSCTL_RESOURCE_NETNAME_REGISTER_DNS_RECORDS      = 0x01000172
+CLUSCTL_RESOURCE_GET_DNS_NAME                      = 0x01000175
+CLUSCTL_RESOURCE_STORAGE_GET_DISK_INFO             = 0x01000191
+CLUSCTL_RESOURCE_STORAGE_IS_PATH_VALID             = 0x01000199
+CLUSCTL_RESOURCE_QUERY_DELETE                      = 0x010001b9
+CLUSCTL_RESOURCE_UPGRADE_DLL                       = 0x014000ba
+CLUSCTL_RESOURCE_IPADDRESS_RENEW_LEASE             = 0x014001be
+CLUSCTL_RESOURCE_IPADDRESS_RELEASE_LEASE           = 0x014001c2
+CLUSCTL_RESOURCE_ADD_REGISTRY_CHECKPOINT_64BIT     = 0x014000be
+CLUSCTL_RESOURCE_ADD_REGISTRY_CHECKPOINT_32BIT     = 0x014000c2
+CLUSCTL_RESOURCE_QUERY_MAINTENANCE_MODE            = 0x010001e1
+CLUSCTL_RESOURCE_SET_MAINTENANCE_MODE              = 0x014001e6
+CLUSCTL_RESOURCE_STORAGE_SET_DRIVELETTER           = 0x014001ea
+CLUSCTL_RESOURCE_STORAGE_GET_DISK_INFO_EX          = 0x010001f1
+CLUSCTL_RESOURCE_FILESERVER_SHARE_ADD              = 0x01400245
+CLUSCTL_RESOURCE_FILESERVER_SHARE_DEL              = 0x01400249
+CLUSCTL_RESOURCE_FILESERVER_SHARE_MODIFY           = 0x0140024d
+CLUSCTL_RESOURCE_FILESERVER_SHARE_REPORT           = 0x01000251
+CLUSCTL_RESOURCE_STORAGE_GET_MOUNTPOINTS           = 0x01000211
+CLUSCTL_RESOURCE_STORAGE_CLUSTER_DISK              = 0x01c00212
+CLUSCTL_RESOURCE_STORAGE_GET_DIRTY                 = 0x01000219
+CLUSCTL_RESOURCE_STORAGE_GET_SHARED_VOLUME_INFO,CLUSCTL_RESOURCE_SET_CSV_MAINTENANCE_MODE = 0x00400296,
+CLUSCTL_RESOURCE_ENABLE_SHARED_VOLUME_DIRECTIO     = 0x0140028a
+CLUSCTL_RESOURCE_DISABLE_SHARED_VOLUME_DIRECTIO    = 0x0140028e
+CLUSCTL_RESOURCE_SET_SHARED_VOLUME_BACKUP_MODE     = 0x0140029a
+CLUSCTL_RESOURCE_DELETE                            = 0x01500006
+CLUSCTL_RESOURCE_INSTALL_NODE                      = 0x0150000a
+CLUSCTL_RESOURCE_EVICT_NODE                        = 0x0150000e
+CLUSCTL_RESOURCE_ADD_DEPENDENCY                    = 0x01500012
+CLUSCTL_RESOURCE_REMOVE_DEPENDENCY                 = 0x01500016
+CLUSCTL_RESOURCE_ADD_OWNER                         = 0x0150001a
+CLUSCTL_RESOURCE_REMOVE_OWNER                      = 0x0150001e
+CLUSCTL_RESOURCE_SET_NAME                          = 0x01500026
+CLUSCTL_RESOURCE_CLUSTER_NAME_CHANGED              = 0x0150002a
+CLUSCTL_RESOURCE_CLUSTER_VERSION_CHANGED           = 0x0150002e
+CLUSCTL_RESOURCE_FORCE_QUORUM                      = 0x01500046
+CLUSCTL_RESOURCE_INITIALIZE                        = 0x0150004a
+CLUSCTL_RESOURCE_STATE_CHANGE_REASON               = 0x0150004e
+CLUSCTL_RESOURCE_PROVIDER_STATE_CHANGE             = 0x01500052
+CLUSCTL_RESOURCE_LEAVING_GROUP                     = 0x01500056
+CLUSCTL_RESOURCE_JOINING_GROUP                     = 0x0150005a
+CLUSCTL_RESOURCE_FSWITNESS_GET_EPOCH_INFO          = 0x0110005d
+CLUSCTL_RESOURCE_FSWITNESS_SET_EPOCH_INFO          = 0x01500062
+CLUSCTL_RESOURCE_FSWITNESS_RELEASE_LOCK            = 0x01500066
+CLUSCTL_RESOURCE_NETNAME_CREDS_UPDATED             = 0x01c0018a
+
 require "Win32API"
 
+#opens
 OpenCluster = Win32API.new('clusapi','OpenCluster',['P'], 'L')
-OpenClusterGroup = Win32API.new('clusapi','OpenClusterGroup',['P','P'], 'L')
+OpenClusterGroup = Win32API.new('clusapi','OpenClusterGroup',['L','P'], 'L')
+OpenClusterResource = Win32API.new('clusapi','OpenClusterResource',['P','P'], 'L')
+
+#closes
+CloseClusterResource = Win32API.new('clusapi','CloseClusterResource',['L'], 'L')
+CloseClusterGroup = Win32API.new('clusapi','CloseClusterGroup',['L'], 'L')
+CloseCluster = Win32API.new('clusapi','CloseCluster',['L'], 'L')
+
+#enums
 ClusterOpenEnum = Win32API.new('clusapi','ClusterOpenEnum',['L','L'], 'L')
 ClusterEnum = Win32API.new('clusapi','ClusterEnum',['L','L','P','P','P'], 'L')
 ClusterGroupOpenEnum = Win32API.new('clusapi','ClusterGroupOpenEnum',['L','L'], 'L')
 ClusterGroupEnum = Win32API.new('clusapi','ClusterGroupEnum',['L','L','P','P','P'], 'L')
 ClusterResourceOpenEnum = Win32API.new('clusapi','ClusterResourceOpenEnum',['L','L'], 'L')
 ClusterResourceEnum = Win32API.new('clusapi','ClusterResourceEnum',['L','L','P','P','P'], 'L')
+
+#create-delete
 CreateClusterGroup = Win32API.new('clusapi','CreateClusterGroup',['L','P'], 'L')
 DeleteClusterGroup = Win32API.new('clusapi','DeleteClusterGroup',['L'], 'L')
 CreateClusterResource = Win32API.new('clusapi','CreateClusterResource',['L','P','P','L'], 'L')
+DeleteClusterResource = Win32API.new('clusapi','DeleteClusterResource',['L'], 'L')
+
+#state
 OnlineClusterGroup = Win32API.new('clusapi','OnlineClusterGroup',['L','L'], 'L')
 OfflineClusterGroup = Win32API.new('clusapi','OfflineClusterGroup',['L'], 'L')
-DeleteClusterResource = Win32API.new('clusapi','DeleteClusterResource',['L'], 'L')
-CloseClusterResource = Win32API.new('clusapi','CloseClusterResource',['L'], 'L')
-CloseClusterGroup = Win32API.new('clusapi','CloseClusterGroup',['L'], 'L')
-CloseCluster = Win32API.new('clusapi','CloseCluster',['L'], 'L')
+
+#resource control
+ClusterResourceControl = Win32API.new('clusapi','ClusterResourceControl',['L','L','L','P','L','P','L','P'], 'L')
+
+
 
 def clus_open(open_type, open_name, cluster_handle=nil)
   open_name = utf8_to_utf16le(open_name)
@@ -73,13 +173,15 @@ def clus_open(open_type, open_name, cluster_handle=nil)
     when 'Cluster'      ; OpenCluster
     when 'Group'        ; OpenClusterGroup
     when 'Resource'     ; OpenClusterResource
-    when 'NetInterface' ; OpenClusterNetInterface
-    when 'Network'      ; OpenClusterNetWork
-    when 'Node'         ; OpenClusterNode
+    # when 'NetInterface' ; OpenClusterNetInterface
+    # when 'Network'      ; OpenClusterNetWork
+    # when 'Node'         ; OpenClusterNode
     end
   end
  
   # only 1 arg needed for OpenCluster, 2 for the others
+  
+  
   open_type == 'Cluster' ? (handle = cluster_open.call(open_name)) : (handle = cluster_open.call(cluster_handle,open_name))
   return handle
   
@@ -116,38 +218,51 @@ def clus_enumeration(enumerationtype, myhandle, dwtype)
 end
 
 
-def clus_group(action, groupname, hCluster)
+def clus_group(action, hCluster, res_grp)
+
   case action
     when "add"
-      groupname = utf8_to_utf16le(groupname)
-      CreateClusterGroup.call(hCluster,groupname)
+      res_grp = utf8_to_utf16le(res_grp)
+      CreateClusterGroup.call(hCluster,res_grp)
     when "remove"
-      hGroup=clus_open('Group', groupname, hCluster)
+      
+      hGroup=clus_open('Group', res_grp, hCluster)
       DeleteClusterGroup.call(hGroup)    # this needs to be the handle of the group not the name. enum groups...
     when "query"
-      #groupname = utf8_to_utf16le(groupname)
-      hGroup=clus_open('Group', groupname, hCluster)
+      
+
+      hGroup=clus_open('Group', res_grp, hCluster)
+      
       clus_enumeration('Group',hGroup, CLUSTER_GROUP_ENUM_CONTAINS)
-    end  
+  end  
 end
   
-def cluster_res(action, res_name, res_type, res_grp)
-    res_type = 'IP Address' if res_type =~ /ip/i
-    res_type = 'Network Name' if ['nn', 'networkname'].include?(res_type.downcase)
-   
-    res_name = utf8_to_utf16le(res_name)
-    res_type = utf8_to_utf16le(res_type)
-    res_grp = utf8_to_utf16le(res_grp)
-    
+def clus_res(action, hCluster, res_name, res_type, res_grp)
+
   case action
     when "add"
+      res_type = 'IP Address' if res_type =~ /ip/i
+      res_type = 'Network Name' if ['nn', 'networkname'].include?(res_type.downcase)
+      
+      res_name = utf8_to_utf16le(res_name)
+      res_type = utf8_to_utf16le(res_type)
+      
+      hGroup=clus_open('Group', res_grp, hCluster)
       hRes = CreateClusterResource.call(hGroup,res_name,res_type,0)
     when "remove"
-      DeleteClusterResource.call(hRes)    
+      hResource=clus_open('Resource', res_name, hCluster)
+      DeleteClusterResource.call(hResource)
     when "query"
-      Cluster_Enumeration(CLUSTER_ENUM_RESOURCE,groupname,CLUSTER_RESOURCE_ENUM_DEPENDS)
+    
+      hResource=clus_open('Resource', res_name, hCluster)
+      
+      # if hResource is a Fix >0 then run this, otherwise irb shits the bed.  we should probably fix all the other calls after opens
+      clus_enumeration('Resource', hResource, CLUSTER_RESOURCE_ENUM_DEPENDS)
     end
 end
+
+#btw - all the shit below is garbage ole shit
+
 
 def cluster_res_ip_props (res_name, res_grp, ip_addr, ip_subnetmask, ip_network, ip_netbios='1')
   puts "#{res_name}"
